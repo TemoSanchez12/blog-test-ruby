546501bff1351f3a33820897ba0a340ce8956bfc
Temo Sanchez <batemo4912@gmail.com>
2024-06-23 09:50:18 -0600

    FEAT: Hide unpublish articles and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,9 @@
 class Article < ApplicationRecord
-  VALID_STATUSES = ['public', 'private', 'archived']
+  include Visible
   
   has_many :comments
   
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-  validates :status, inclusion: { in: VALID_STATUSES }
 
-  def archived?
-    status == 'archived'
-  end
-  
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,6 @@
 class Comment < ApplicationRecord
-  VALID_STATUSES = ['public', 'private', 'archived']
+  include Visible
   
   belongs_to :article
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
-
+  
 end
